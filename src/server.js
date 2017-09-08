e71df10ba9c95ba1f2abb2afae65880b53b30340
Drew Jaqua <anjaqua@gmail.com>
2017-09-07 22:51:32 -0400

    (S1005) - added ability to serve HTML without CSS formatting.

--- src/server.js
+++ src/server.js
@@ -8,15 +8,19 @@ const express = require('express');
 const conf = require("./configuration");
 const logger = require("./logging").getLogger();
 logger.notice("Using top level configuration file '" + conf('filename') + "'");
-
+const path = require('path');
 const app = express();
 const port = conf('service.port');
 
+//app.use(express.static('src/public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+/*
 app.get('/', (req, res) => {
     logger.debug('GET / requested');
     res.send("Hello, world!");
 });
-
+*/
 app.listen(port, (err) => {
     if (err) {
         logger.error("Something went wrong: ", err);
