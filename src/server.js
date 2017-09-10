f71aa9063f76e6d3cc7bf6f03b4d50ee52e09666
Drew Jaqua <anjaqua@gmail.com>
2017-09-09 22:04:42 -0400

    (S1007) Cleaned up formatting

--- src/server.js
+++ src/server.js
@@ -23,71 +23,71 @@ logger.notice("Using top level config.getiguration file '" + conf('filename') + 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/delete-them-all', (req, res) => {
-    logger.debug('GET /delete-them-all requested');
+  logger.debug('GET /delete-them-all requested');
 
-    let allTodos = persistence.getAllTodos();
-    logger.debug("All todos: " + allTodos);
-    allTodos.exec((err, docs) => {
-        if (err) {
-            logger.error(err);
-        }
-        _.forEach( docs, (doc) => {
-            persistence.removeTodo(doc._id);
-        });
+  let allTodos = persistence.getAllTodos();
+  logger.debug("All todos: " + allTodos);
+  allTodos.exec((err, docs) => {
+    if (err) {
+      logger.error(err);
+    }
+    _.forEach( docs, (doc) => {
+      persistence.removeTodo(doc._id);
     });
+  });
 
-    res.send(theMenu);
+  res.send(theMenu);
 });
 
 app.get('/read-them-all', (req, res) => {
-    logger.debug('GET /read-them-all requested');
+  logger.debug('GET /read-them-all requested');
 
-    let allTodos = persistence.getAllTodos();
-    logger.debug("All todos: " + allTodos);
-    allTodos.exec((err, docs) => {
-        if (err) {
-            logger.error(err);
-        }
-        _.forEach( docs, (doc) => {
-            logger.info(doc.text);
-        });
+  let allTodos = persistence.getAllTodos();
+  logger.debug("All todos: " + allTodos);
+  allTodos.exec((err, docs) => {
+    if (err) {
+      logger.error(err);
+    }
+    _.forEach( docs, (doc) => {
+      logger.info(doc.text);
     });
+  });
 
-    res.send(theMenu);
+  res.send(theMenu);
 });
 
 
 
 app.get('/create-one', (req, res) => {
-    let foo = persistence.addTodo("Keep on keep'n on");
-    logger.debug('GET /create-one requested');
-    if (foo) {
-        logger.debug("created Todo item with id: " + foo._id);
-    }
-    else {
-        logger.error(err);
-    }
-    res.send(theMenu);
+  let foo = persistence.addTodo("Keep on keep'n on");
+  logger.debug('GET /create-one requested');
+  if (foo) {
+    logger.debug("created Todo item with id: " + foo._id);
+  }
+  else {
+    logger.error(err);
+  }
+  res.send(theMenu);
 });
 
 
 app.get('/complete-one', (req, res) => {
-    let foo = persistence.addTodo("Keep on keep'n on");
-    logger.debug('GET /complete-one requested');
-    if (foo) {
-        logger.debug("created Todo item with id: " + foo._id);
-    }
-    else {
-        logger.error(err);
-    }
-    res.send(theMenu);
+  let foo = persistence.addTodo("Keep on keep'n on");
+  logger.debug('GET /complete-one requested');
+  if (foo) {
+    logger.debug("created Todo item with id: " + foo._id);
+  }
+  else {
+    logger.error(err);
+  }
+  res.send(theMenu);
 });
 
 
 app.listen(port, (err) => {
-    if (err) {
-        logger.error("Something went wrong: ", err);
-        return;
-    }
-    logger.notice("Server is listening on ", port);
+  if (err) {
+    logger.error("Something went wrong: ", err);
+    return;
+  }
+  logger.notice("Server is listening on ", port);
 });
