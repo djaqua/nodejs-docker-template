6cbfdac060892e5e04119642c353089425f744e2
Drew Jaqua <djaqua@gmail.com>
2017-11-30 01:18:21 -0500

    (master) cleaned up the back-end substantially; can now create, complete, edit, and view items in the todo list

--- src/persistence.js
+++ src/persistence.js
@@ -1,81 +1,29 @@
-const mongoose = require('mongoose');
-const conf = require('./configuration');
+
+const conf = require('./configuration')('persistance.mongo');
 const logger = require('./logging');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
-
-const dbconf = conf('persistance.mongo');
-
-Promise = require('bluebird');
-
-mongoose.Promise = Promise;
-logger.debug('set to bluebird')
-
-const mongoUri = dbconf.hostname;
-
-mongoose.connect('mongodb://' + dbconf.hostname + ':' + dbconf.port + '/' + dbconf.dbname, {
+mongoose.connect(`mongodb://${conf.hostname}:${conf.port}/${conf.dbname}`, {
   useMongoClient: true,
-  user: dbconf.username,
-  pass: dbconf.password
+  user: conf.username,
+  pass: conf.password
 })
 .then(() => {
   logger.info("Successfully established a connection to mongo");
 })
 .catch(err => {
-  logger.error("There was an error connecting to mongo: " + err);
+  logger.error(`There was an error connecting to mongo: ${err}`);
   process.exit(1);
 });
 
-logger.debug('mongoose.connection.readyState=' + mongoose.connection.readyState);
+logger.debug(`mongoose.connection.readyState=${mongoose.connection.readyState}`);
 
-const Todo = mongoose.model('Todo', {
-  text: String,
-  completed: Date,
-  created: { type: Date, default: Date.now }
-});
 
 module.exports = {
-
-  /* The 'C' in CRUD */
-  addTodo: function(todoText) {
-    let todo = new Todo({text: todoText});
-    todo.save(function(err, obj) {
-      if (err) {
-        logger.error(err);
-      }
-    });
-    return todo;
-  },
-
-  /* The 'R' in CRUD */
-  getAllTodos: function() {
-    return Todo.find(function(err, docs) {
-      if (err) {
-        logger.error(err);
-      }
-    });
-  },
-
-  /* The 'U' in CRUD */
-  completeTodo: function(todoId) {
-    let retVal = null;
-    Todo.update(todoId, {completed: new Date()}, (err, todoModel) => {
-      if (err) {
-        logger.error(err);
-      }
-      retVal = todoModel;
-    });
-    return retVal;
-  },
-
-  /* The 'D' in CRUD */
-  removeTodo: function(todoId) {
-    Todo.deleteOne({
-      _id:todoId
-    }, function(err, todoModel) {
-      if (err) {
-        logger.error(err);
-      };
-      retVal = todoModel;
-    });
-  }
+  Todo: mongoose.model('Todo', {
+    text: String,
+    completed: Date,
+    created: { type: Date, default: Date.now }
+  })
 }
--- src/server.js
+++ src/server.js
@@ -5,83 +5,91 @@
  */
 const conf = require('./configuration');
 const logger = require('./logging');
-const persistence = require('./persistence');
+const {
+  Todo
+} = require('./persistence');
 
 const _ = require('lodash');
 const express = require('express');
 const path = require('path');
-
-
 const app = express();
 const port = conf('service.port');
-const theMenu = '[<a href=\'/create-one\'>create one</a>]' +
-                '[<a href=\'/read-them-all\'>read them all</a>]' +
-                '[<a href=\'/delete-them-all\'>delete them all</a>]';
 
 logger.notice(`Using top level configuration file ${conf('filename')}`);
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/delete-them-all', (req, res) => {
-  logger.debug('GET /delete-them-all requested');
-
-  let allTodos = persistence.getAllTodos();
-  allTodos.exec((err, docs) => {
-    if (err) {
-      logger.error(err);
-    }
-    _.forEach( docs, (doc) => {
-      persistence.removeTodo(doc._id);
-    });
-  });
-
-  res.send(theMenu);
-});
-
-app.get('/read-them-all', (req, res) => {
-  logger.debug('GET /read-them-all requested');
-
-  let allTodos = persistence.getAllTodos();
-  allTodos.exec((err, docs) => {
+app.get('/all', (req, res) => {
+  logger.debug('GET /all requested');
+  Todo.find({}, function (err, docs) {
     if (err) {
       logger.error(err);
     }
     res.send(JSON.stringify(docs));
 
-    _.forEach( docs, (doc) => {
-      logger.debug(doc.text);
-    });
-    return;
   });
 });
 
+app.post('/create/:text', (req, res) => {
+  logger.debug('POST /create requested');
+  const text = req.params.text;
 
+  let todo = new Todo({text: text});
+  todo.save(function(err, obj) {
+    if (err) {
+        res.send(`{"error":"${err}"}`);
+    }
+    res.send(`{"result":"ok"}`);
+  });
 
-app.get('/create-one', (req, res) => {
-  logger.debug('GET /create-one requested');
-
-  let foo = persistence.addTodo('Keep on keep\'n on');
-  if (foo) {
-    logger.debug(`created Todo item with id: ${foo._id}`);
-  }
-  else {
-    logger.error(err);
-  }
-  res.send(theMenu);
 });
 
+app.put('/updateText/:id/:text', (req, res) => {
 
-app.get('/complete-one', (req, res) => {
-  logger.debug('GET /complete-one requested');
+  logger.debug('PUT /updateText requested');
 
-  let foo = persistence.completeTodo(req.query.id);
-  if (foo) {
-    logger.debug(`completed Todo item with id: ${foo._id}`);
-  }
+  const {
+    id,
+    text
+  } = req.params;
 
-  res.send(theMenu);
+  logger.debug("req.params.id: " + id);
+  logger.debug("req.params.text: " + text);
+
+  Todo.findById(id, function (err, doc) {
+    if (err) {
+      logger.error(err);
+      res.send(`{"error":"${err}"}`);
+    }
+    else {
+      doc.text = text;
+      doc.save();
+      res.send(`{"result":"ok"}`);
+    }
+  });
 });
 
+app.put('/complete/:id', (req, res) => {
+
+  logger.debug('PUT /complete requested');
+
+  const id = req.params.id;
+
+  logger.debug("req.params.id: " + id);
+
+  Todo.findById(id, function (err, doc) {
+
+    if (err) {
+      logger.error(err);
+      res.send(`{"error":"${err}"}`);
+    }
+    else {
+      doc.completed = new Date();
+      doc.save();
+      res.send(`{"result":"ok"}`);
+    }
+  });
+});
 
 app.listen(port, (err) => {
   if (err) {
